d51bb39c41c998d241934de5b699b0dd42f4ada9
Lucas Dal Jovem <lucasdaljovem@gmail.com>
2024-05-26 22:01:11 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 //imports here
+import React from 'react';
 
+import NavbarComponent from './assets/navbar/NavbarComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      {/* code */}
+      <div>
+        <NavbarComponent />
+        <div className="container mt-5">
+          <h1>Conteúdo da Página</h1>
+          {/* Resto do conteúdo da página */}
+        </div> {/* Esta div estava faltando a tag de fechamento */}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
+const container = document.getElementById('root'); // Certifique-se de que o id corresponde ao que está no HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
